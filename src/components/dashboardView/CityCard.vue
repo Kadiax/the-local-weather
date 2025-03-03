67d52fd4c4f88baccbaf09504fbaf2e004256ecd
Kadiax <kadia.toure.pro@gmail.com>
2025-03-03 12:17:17 +0100

    fix unused import

--- src/components/dashboardView/CityCard.vue
+++ src/components/dashboardView/CityCard.vue
@@ -2,9 +2,7 @@
   <div class="card">
     <div class="weather-info">
       <div class="figures">
-        <div class="today-temp">
-          {{ roundedTemperature(city.weather.main.temp) }}&deg;
-        </div>
+        <div class="today-temp">{{ roundedTemperature(city.weather.main.temp) }}&deg;</div>
         <div class="today-high-low">
           H{{ roundedTemperature(city.weather.main.temp_max) }}&deg; L{{
             roundedTemperature(city.weather.main.temp_min)
@@ -13,28 +11,23 @@
       </div>
       <div class="city-name">{{ city.city }}</div>
     </div>
-    <img
-      v-if="weatherIconUrl"
-      :src="weatherIconUrl(city.weather.weather[0].icon)"
-      alt="Weather Icon"
-    />
+    <img v-if="weatherIconUrl" :src="weatherIconUrl(city.weather.weather[0].icon)" alt="Weather Icon" />
   </div>
 </template>
 
 <script setup>
-import { computed } from "vue";
-import { roundedTemperature } from "@/helpers/roundedTemperature";
-import { weatherIconUrl } from "@/helpers/weatherIconUrl";
+  import { roundedTemperature } from '@/helpers/roundedTemperature';
+  import { weatherIconUrl } from '@/helpers/weatherIconUrl';
 
-const props = defineProps({
-  city: {
-    type: Object,
-    default: () => ({}),
-    required: true,
-  },
-});
+  defineProps({
+    city: {
+      type: Object,
+      default: () => ({}),
+      required: true
+    }
+  });
 </script>
 
 <style scoped>
-@import "@/assets/styles/dashboardView/city-card.css";
+  @import '@/assets/styles/dashboardView/city-card.css';
 </style>
