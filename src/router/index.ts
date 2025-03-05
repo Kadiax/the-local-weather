d93b39b2bb4ab4e37d6347a31660a2a4810abe55
Kadiax <kadia.toure.pro@gmail.com>
2025-03-05 12:27:37 +0100

    fix beforeEach unused variable

--- src/router/index.ts
+++ src/router/index.ts
@@ -27,7 +27,7 @@ const router = createRouter({
   ]
 });
 
-router.beforeEach((to, from, next) => {
+router.beforeEach((to, _from, next) => {
   document.title = `${to.params.state ? `${to.params.city}, ${to.params.state}` : to.meta.title} | The Local Weather`;
   next();
 });
